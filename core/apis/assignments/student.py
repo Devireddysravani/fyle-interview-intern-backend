8f091928969187fbfd7103cf43fb3c7fb2c17909
Devireddysravani <135237206+Devireddysravani@users.noreply.github.com>
2024-01-20 13:03:20 +0530

    Update student.py

--- core/apis/assignments/student.py
+++ core/apis/assignments/student.py
@@ -5,11 +5,12 @@ from core.apis.responses import APIResponse
 from core.models.assignments import Assignment
 
 from .schema import AssignmentSchema, AssignmentSubmitSchema
+
 student_assignments_resources = Blueprint('student_assignments_resources', __name__)
 
 
 @student_assignments_resources.route('/assignments', methods=['GET'], strict_slashes=False)
-@decorators.authenticate_principal
+@decorators.auth_principal
 def list_assignments(p):
     """Returns list of assignments"""
     students_assignments = Assignment.get_assignments_by_student(p.student_id)
@@ -19,7 +20,7 @@ def list_assignments(p):
 
 @student_assignments_resources.route('/assignments', methods=['POST'], strict_slashes=False)
 @decorators.accept_payload
-@decorators.authenticate_principal
+@decorators.auth_principal
 def upsert_assignment(p, incoming_payload):
     """Create or Edit an assignment"""
     assignment = AssignmentSchema().load(incoming_payload)
@@ -33,7 +34,7 @@ def upsert_assignment(p, incoming_payload):
 
 @student_assignments_resources.route('/assignments/submit', methods=['POST'], strict_slashes=False)
 @decorators.accept_payload
-@decorators.authenticate_principal
+@decorators.auth_principal
 def submit_assignment(p, incoming_payload):
     """Submit an assignment"""
     submit_assignment_payload = AssignmentSubmitSchema().load(incoming_payload)
@@ -41,8 +42,37 @@ def submit_assignment(p, incoming_payload):
     submitted_assignment = Assignment.submit(
         _id=submit_assignment_payload.id,
         teacher_id=submit_assignment_payload.teacher_id,
-        auth_principal=p
+        principal=p
     )
     db.session.commit()
     submitted_assignment_dump = AssignmentSchema().dump(submitted_assignment)
     return APIResponse.respond(data=submitted_assignment_dump)
+
+
+@student_assignments_resources.route('/assignments/<int:assignment_id>', methods=['GET'], strict_slashes=False)
+@decorators.auth_principal
+def get_assignment(p, assignment_id):
+    assignment = Assignment.get_assignment_by_id(assignment_id, p.student_id)
+    if assignment:
+        assignment_dump = AssignmentSchema().dump(assignment)
+        return APIResponse.respond(data=assignment_dump)
+    else:
+        return APIResponse.respond(message="Assignment not found", status_code=404)
+
+
+@student_assignments_resources.route('/assignments/drafts', methods=['GET'], strict_slashes=False)
+@decorators.auth_principal
+def list_draft_assignments(p):
+    """List draft assignments"""
+    draft_assignments = Assignment.get_draft_assignments_by_student(p.student_id)
+    draft_assignments_dump = AssignmentSchema().dump(draft_assignments, many=True)
+    return APIResponse.respond(data=draft_assignments_dump)
+
+
+@student_assignments_resources.route('/assignments/submitted', methods=['GET'], strict_slashes=False)
+@decorators.auth_principal
+def list_submitted_assignments(p):
+    """List submitted assignments"""
+    submitted_assignments = Assignment.get_submitted_assignments_by_student(p.student_id)
+    submitted_assignments_dump = AssignmentSchema().dump(submitted_assignments, many=True)
+    return APIResponse.respond(data=submitted_assignments_dump)
